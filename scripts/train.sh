44ea6ed95234325ebc478b3e25cf98ed62165e5a
jihoon <jihoon.kwon@getlinq.com>
2024-09-26 20:38:11 +0900

    update code

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,13 +3,12 @@ export HF_HOME="TBD"
 export LOG_DIR="TBD"
 export DATA_DIR="TBD" # Add for dataset caching
 export OPENAI_API_KEY="TBD"
-export TRAINING_VERBOSITY="detail"
 
 DEVICES=0
 NUM_TRAINERS=1
 CUDA_VISIBLE_DEVICES=$DEVICES torchrun \
     --standalone \
     --nproc_per_node=$NUM_TRAINERS \
-    /mnt/working/multimodal/train.py \
-        --cfg-path '/mnt/working/multimodal/configs/train/negclip.yml' \
+    /mnt/working/financialann/train.py \
+        --cfg-path 'TBD.yml' \
         --wandb-key '3314a9f18c06914b9c333abc68130f93f2cb1a23'
